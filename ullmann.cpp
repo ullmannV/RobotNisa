4910c4019d1a569b6868ab6f5e2b9baf1b5930bf
ullmannV <ullmann.vojta@gmail.com>
2020-01-07 23:12:58 +0100

    Generator taktu

--- ullmann.cpp
+++ ullmann.cpp
@@ -9,22 +9,61 @@
 
 
 
+
+
+
 int main(void) {
-    // inicializace
+    // priprava programu
+    
+    // reset po predchozim behu programu
     clrscr(); // vycisti plochu
 
+    unsigned char output_buffer = VSE_VYPNUTO;
+
+    outportb(port_vystupy, output_buffer);
+
+    // podminka behu programu
     bool program_run = true;
 
-    // nekonecny cyklus
+    // prevedeni rychlosti v Hz na ms
+    const double perioda_taktu_ms = taktovaci_frekvence_robota < MAX_RYCHLOST_ROBOTA ? SEC_TO_MILISEC_COEFFICIENT/taktovaci_frekvence_robota : SEC_TO_MILISEC_COEFFICIENT/MAX_RYCHLOST_ROBOTA; 
+    
+    // Promenne "stopek" pro generovani taktu
+    clock_t last_time = clock();
+    clock_t now_time;
+
+    // nekonecny cyklus - hlavni cast programu
     while(program_run) {
         
-        // zmena chovani programu pomoci vstupu z klavesnice
-        switch() { // TODO vstup klavesy
-            case 'q': {
-                program_run = false;
-                break;
-            }
-        } 
+        // taktovaci bit - generovani signalu 
+        now_time = clock(); // aktualizace informace o case
+
+        // vyhodnoceni zda ma dojit k preklopeni taktu
+        if((((double) now_time - last_time) / CLOCKS_PER_SEC) * SEC_TO_MILISEC_COEFFICIENT >= perioda_taktu_ms) {
+            last_time = now_time;
+            output_buffer ^= 1<<BIT_TAKT;   // vytvoreni 1:1 (50%) stridy 
+        }
+        
+        // zmena chovani programu pri vstupu z klavesnice
+        if(kbhit()) {
+            int key = getch(); // ziskani stiskle klavesy
+
+            // reakce pouze na pozadovane klavesy
+            switch(key) {
+                case 'q': {
+                    program_run = false;
+                    break;
+                }
+                default: {
+                    printf("Stisknuta nic nedelajici klavesa \n");
+                    continue;
+                }
+            } 
+        }
+        else {
+            // TODO Vypnuti motoru
+        }
+        
     }
     printf("Program vypnut \n");
     return 0;
--- ullmann.hpp
+++ ullmann.hpp
@@ -1,5 +1,13 @@
 #ifndef ULLMANN_HPP
 #define ULLMANN_HPP
 
+#define MAX_RYCHLOST_ROBOTA 450 // Hz
+#define VSE_VYPNUTO 0xFF // periferie jsou aktivni v logicke nule
+#define SEC_TO_MILISEC_COEFFICIENT 1000 // 1 s = 1000 ms
+#define BIT_TAKT 5
+
+const unsigned short port_vystupy = 0x300; // P1
+const unsigned short port_vstupy = 0x300;  // P3
+const unsigned short taktovaci_frekvence_robota = 100; // Hz
 
 #endif
