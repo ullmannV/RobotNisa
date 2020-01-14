9ff1f5de1ec9bbfa07e158cd8a5c92d8cae47371
ullmannV <ullmann.vojta@gmail.com>
2020-01-14 21:38:07 +0100

    Control Switch

--- ullmann.c
+++ ullmann.c
@@ -2,26 +2,48 @@
 #include <stdio.h>
 #include <dos.h>
 #include <conio.h>
-#include "booleanDOS.h"
 #include "ullmann.h"
 
 // pomocne konstanty
 const unsigned char VSE_VYPNUTO = 0xFF; // periferie jsou aktivni v logicke nule
 const unsigned short SEC_TO_MILISEC_COEFFICIENT = 1000; // 1 s = 1000 ms
 
+// pole klaves k ovladani
+const char control_keys[] = 
+{
+    'q', // Vypnuti programu
+    'a', // Otoceni zakladny proti smeru hodinovych rucicek
+    'd', // Otoceni zakladny po smeru hodinovych rucicek
+    'w', // Zvednuti hlavniho ramene
+    's', // Snizeni hlavniho ramene
+    'r', // Zvednuti ramene celisti
+    'f', // Snizeni ramene celisti
+    'x', // Zavreni celisti
+    'c'  // Otevreni celisti
+};
+
 // Zapojeni
-const unsigned char BIT_ZAKLDNA;
-const unsigned char BIT_HLAVNI_RAMENO;
-const unsigned char BIT_RAMENO_CELISTI;
-const unsigned char BIT_CELIST;
-const unsigned char BIT_TAKT = 5;
-const unsigned char BIT_SMER;
+/*
+    Robot ma vstupy i vystupy, nicmene jej zapojime tak, aby
+    si vahy bitu odpovidaly s danym vstupem i vystupem
+    Priklad: 
+    BIT_ZAKLADNA = 0 -> motor zakladny je zapojen na VYSTUPNIM portu na bitu 0
+                     -> zavora zakladny je zapojena na VSTUPNIM portu na bitu 0 
+*/
+const unsigned char BIT_ZAKLDNA = 0;
+const unsigned char BIT_HLAVNI_RAMENO = 1;
+const unsigned char BIT_RAMENO_CELISTI = 2;
+const unsigned char BIT_CELIST = 3;
+const unsigned char BIT_TAKT = 4;
+const unsigned char BIT_SMER = 5;
 
 // konstrukcni konstanty
-const unsigned short PORT_OUT = 0x300; // P1
-const unsigned short PORT_IN = 0x300;  // P3
-const unsigned short RYCHLOST_ROBOTA = 100; // Hz
+const unsigned short PORT_OUT = 0x300; // Port P1
+const unsigned short PORT_IN = 0x300;  // Port P3
 const unsigned short MAX_RYCHLOST_ROBOTA = 450; // Hz
+
+// volitelne parametry:
+const unsigned short RYCHLOST_ROBOTA = 100; // Hz
 
 int main(void) {
     // priprava programu
@@ -34,7 +56,7 @@ int main(void) {
     outportb(port_vystupy, output_buffer); // motory vypnuty
 
     // podminka behu programu
-    BOOL program_run = true;
+    bool program_run = true;
 
     // prevedeni rychlosti v Hz na ms
     const unsigned int perioda_taktu_ms = taktovaci_frekvence_robota < MAX_RYCHLOST_ROBOTA ? SEC_TO_MILISEC_COEFFICIENT/taktovaci_frekvence_robota : SEC_TO_MILISEC_COEFFICIENT/MAX_RYCHLOST_ROBOTA; 
@@ -48,37 +70,37 @@ int main(void) {
 
             // reakce na dane stiskle klavesy
             switch(pressed_key) {
-                case 'q':       // Vypnuti programu
+                case control_keys[0]:       // Vypnuti programu
                     program_run = false;
                     break;      
-                case 'a':       // Otoceni zakladny proti smeru hodinovych rucicek
+                case control_keys[1]:       // Otoceni zakladny proti smeru hodinovych rucicek
                     break;
-                case 'd':       // Otoceni zakladny po smeru hodinovych rucicek
+                case control_keys[2]:       // Otoceni zakladny po smeru hodinovych rucicek
                     break;
-                case 'w':       // Zvednuti hlavniho ramene
+                case control_keys[3]:       // Zvednuti hlavniho ramene
                     break;
-                case 's':       // Snizeni hlavniho ramene
+                case control_keys[4]:       // Snizeni hlavniho ramene
                     break;
-                case 'r':       // Zvednuti ramene celisti
+                case control_keys[5]:       // Zvednuti ramene celisti
                     break;
-                case 'f':       // Snizeni ramene celisti
+                case control_keys[6]:       // Snizeni ramene celisti
                     break;      
-                case 'x':       // Zavreni celisti
+                case control_keys[7]:       // Zavreni celisti
                     break;
-                case 'c':       // Otevreni celisti
+                case control_keys[8]:       // Otevreni celisti
                     break;
                 default: 
                     printf("Stisknuta neznama klavesa.\n");
-                    break;
+                    continue;
             }
-        }
+        } /*End of Switch*/
         
         output_buffer ^= 1<<BIT_TAKT; // Toggle taktovaciho bitu 
 
         outportb(port_vystupy, output_buffer); // odeslani zpracovanych dat na vystupni piny portu
         delay(perioda_taktu_ms); // Zpozdeni pro generovani taktu
 
-    } while(program_run);
+    } while(program_run); /*End of Main Infinite Loop*/ 
     
     printf("Program vypnut \n");
     return 0;
